65941a1575c84890d852194558d60084eda91cfd
Rohit Kumar Gupta <rohitgupta.dbs@gmail.com>
2020-04-30 09:45:11 +0530

    Updates

--- README.md
+++
@@ -1,4 +0,0 @@
-This project based on React Js that
-Is Creation Of Tic Tac Toe Game 
-As well As Todo list. You  Can See 
-My code for Both that has been Posted above. 
---
+++ precache-manifest.4f0f40d1ef811bfeb21e27963a73d629.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52827919daa8b989133a8476f188d13a",
+    "url": "/Todolist/index.html"
+  },
+  {
+    "revision": "210d16c5427164fe18fe",
+    "url": "/Todolist/static/css/main.7f4dd3ea.chunk.css"
+  },
+  {
+    "revision": "ad44c8676f93c0d3b105",
+    "url": "/Todolist/static/js/2.189c6b91.chunk.js"
+  },
+  {
+    "revision": "210d16c5427164fe18fe",
+    "url": "/Todolist/static/js/main.68e0f580.chunk.js"
+  },
+  {
+    "revision": "6a2b389b0dc3d1d155bf",
+    "url": "/Todolist/static/js/runtime~main.0e89f3c6.js"
+  },
+  {
+    "revision": "541c2c9fc892c05a3546d401c77c4170",
+    "url": "/Todolist/static/media/images (3).541c2c9f.jpg"
+  },
+  {
+    "revision": "9a3ada5c3405512f0387b733ccc56251",
+    "url": "/Todolist/static/media/images (4).9a3ada5c.jpg"
+  }
+]);
